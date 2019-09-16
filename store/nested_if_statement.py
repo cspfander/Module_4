e83d13403f23fea17654fef4cf9cac0f2685a881
packe <cspfander@dmacc.edu>
2019-09-16 13:56:39 -0500

    Initial Commit

---
+++ store/nested_if_statement.py
@@ -0,0 +1,14 @@
+"""
+Program: nested_if_statement.py
+Author: Colten Pfander
+Last date modified: 9/16/2019
+
+
+
+The purpose of this program is to write a function that will accept the amount of the purchase,
+the cash coupon, the percent coupon, and calculate and return the total order item.
+"""
+def calculate_order(price, cash_coupon, percent_coupon):
+    pass
+
+if __name__ == '__main__':
