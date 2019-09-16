b5c547c907a3537b4c58465299cb2fcaf568afd3
packe <cspfander@dmacc.edu>
2019-09-16 18:23:40 -0500

    Added 2nd ValueError Test - Failed

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -7,9 +7,13 @@ class MyTestCase(unittest.TestCase):
     def test_average(self):
         self.assertEqual(validation_with_try.average(85, 90, 95), 90)
 
-    def test_average_negative_input(self):
+    def test_average_exception_one(self):
         with self.assertRaises(ValueError):
             validation_with_try.average(-90, 89, 78)
+
+    def test_average_exception_two(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90, -89, 78)
 
 
 if __name__ == '__main__':
