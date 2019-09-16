b91785e2650bf7e5476afcd7df93fd4dd9c9db98
packe <cspfander@dmacc.edu>
2019-09-16 14:37:53 -0500

    Test set 1 Passed

--- store/nested_if_statement.py
+++ store/nested_if_statement.py
@@ -8,7 +8,23 @@ Last date modified: 9/16/2019
 The purpose of this program is to write a function that will accept the amount of the purchase,
 the cash coupon, the percent coupon, and calculate and return the total order item.
 """
+
+
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    tax_rate = 0.06
+    total_order = 0
+    if price < 10:
+        shipping = 5.95
+        if cash_coupon == 5:
+            price = price - 5
+            price = price - (price * percent_coupon)
+            total_order = price + (price * tax_rate) + shipping
+            return round(total_order, 2)
+        if cash_coupon == 10:
+            price = 0
+            total_order = price + shipping
+            return round(total_order, 2)
+
 
 if __name__ == '__main__':
+    print(calculate_order())
--- test/nested_if_test.py
+++ test/nested_if_test.py
@@ -4,22 +4,22 @@ from store import nested_if_statement
 
 class MyTestCase(unittest.TestCase):
     def test_price_under_10_5_off_10_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, 10), 9.75)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, .10), 9.76)
 
     def test_price_under_10_5_off_15_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, 15), 9.54)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, .15), 9.54)
 
     def test_price_under_10_5_off_20_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, 20), 9.33)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 5, .20), 9.33)
 
     def test_price_under_10_10_off_10_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, 10), 5.95)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, .10), 5.95)
 
     def test_price_under_10_10_off_15_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, 15), 5.95)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, .15), 5.95)
 
     def test_price_under_10_10_off_20_percent(self):
-        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, 20), 5.95)
+        self.assertEqual(nested_if_statement.calculate_order(8.99, 10, .20), 5.95)
 
 
 if __name__ == '__main__':
